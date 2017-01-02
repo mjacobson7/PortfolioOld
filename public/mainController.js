2356c30ec1dc4e5104ee3af14b83ed9fda6e6353
maxjacobson <max.jacobson@torchlms.com>
2017-01-02 15:24:59 -0700

    Changed scripts to .min files

--- public/mainController.js
+++ public/mainController.js
@@ -1,57 +1,5 @@
 angular.module('myPortfolio', [])
   .controller('mainController', function($scope, $interval, $timeout) {
 
-    function repeat() {
-
-      $timeout(function() {
-        $scope.test1 = 'test';
-      }, 500);
-      $timeout(function() {
-        $scope.test1 = 'remove';
-      }, 8500);
-
-      $timeout(function() {
-        $scope.test2 = 'test';
-      }, 9500);
-      $timeout(function() {
-        $scope.test2 = 'remove';
-      }, 18500);
-
-      $timeout(function() {
-        $scope.test3 = 'test';
-      }, 19500);
-      $timeout(function() {
-        $scope.test3 = 'remove';
-      }, 28500);
-
-    $interval(function() {
-
-    $timeout(function() {
-      $scope.test1 = 'test';
-    }, 500);
-    $timeout(function() {
-      $scope.test1 = 'remove';
-    }, 8500);
-
-    $timeout(function() {
-      $scope.test2 = 'test';
-    }, 9500);
-    $timeout(function() {
-      $scope.test2 = 'remove';
-    }, 18500);
-
-    $timeout(function() {
-      $scope.test3 = 'test';
-    }, 19500);
-    $timeout(function() {
-      $scope.test3 = 'remove';
-    }, 28500);
-
-  }, 29500);
-}
-
-repeat();
-
-
 
   }); //end
