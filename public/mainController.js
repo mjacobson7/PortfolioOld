48706ab0a639e5ff16fbdacb7bdb92beec68a014
maxjacobson <max.jacobson@torchlms.com>
2017-01-04 22:26:42 -0700

    added nodemailer functionality

--- public/mainController.js
+++ public/mainController.js
@@ -1,5 +1,21 @@
 angular.module('myPortfolio', [])
-  .controller('mainController', function($scope, $interval, $timeout) {
+  .controller('mainController', function($scope, $http) {
+
+    $scope.sendMessage = function(user) {
+      return $http({
+        method: 'POST',
+        url: '/api/sendmail',
+        data: user
+      }).then(function(response) {
+        // swal("You Rock!", "Your message has been sent!", "success");
+        swal({
+          title: 'You Rock!',
+          text: "Your message has been sent!",
+          type: 'success',
+          confirmButtonColor: '#35a49d',
+        })
+      })
+    }
 
 
   }); //end
--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 var app = require('./config/express.js')();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport()
+
 var port = process.env.PORT || 7000;
 
 app.listen(port, function() {
   console.log("Now listening to port number:", port);
 })
+
+
+app.post('/api/sendmail', function(req, res) {
+  var user = req.body;
+
+  transporter.sendMail({
+    from: '"maxjacobson.me" <no-reply@maxjacobson.me>', // sender address
+    to: 'mjacobson7@gmail.com', // list of receivers
+    subject: 'New message from ' + user.name + '!', // Subject line
+    html: '<p>' + user.message + '<p>' + '</br>' + '<p>' + user.email + '<p>'
+  })
+
+  res.status(200).json(user);
+
+});
