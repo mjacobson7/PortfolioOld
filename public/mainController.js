e38b1dd739b88cb3e7789746990f38c74b742502
maxjacobson <max.jacobson@torchlms.com>
2017-01-08 21:28:31 -0700

    Added form validation logic

--- public/mainController.js
+++ public/mainController.js
@@ -1,12 +1,17 @@
 angular.module('myPortfolio', ['ngTouch'])
   .controller('mainController', function($scope, $http) {
 
-    $scope.sendMessage = function(user) {
+    var sendMessage = function(user) {
       return $http({
         method: 'POST',
         url: '/api/sendmail',
         data: user
       }).then(function(response) {
+        $scope.user.name = "";
+        $scope.user.email = "";
+        $scope.user.message = "";
+        $scope.userForm.$setPristine();
+        $scope.submitted = false;
         swal({
           title: 'You Rock!',
           text: "Your message has been sent!",
@@ -14,6 +19,14 @@ angular.module('myPortfolio', ['ngTouch'])
           confirmButtonColor: '#35a49d',
         })
       })
+    }
+
+
+    $scope.submitForm = function(isValid) {
+      $scope.submitted = true;
+      if(isValid) {
+        return sendMessage($scope.user);
+      }
     }
 
 
